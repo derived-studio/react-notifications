1641aa78f717eef56829046b73758cf4b84ab58b
Mark Bevels <mark.bevels@derived.studio>
2021-03-06 14:29:07 +0000

    Cleanup

--- src/lib/Notification.tsx
+++ src/lib/Notification.tsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 
+export interface INotification {
+  readonly id: string
+  readonly message: string
+  readonly created: string
+  readonly type: string
+}
+
 export type NotificationProps = {
   message: string
 }
