9d617ebb2dcc1cf90682345ffd01bfacceee5938
Mark Bevels <mark.bevels@derived.studio>
2021-03-08 11:53:36 +0000

    Fixing class props errors with  @babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,24 @@
 module.exports = function (api) {
   api.cache(true)
 
-  const presets = [
-    [
-      '@babel/preset-react',
-      {
-        runtime: 'automatic'
-      }
+  return {
+    presets: [
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic'
+        }
+      ],
+      '@babel/typescript',
+      [
+        '@babel/preset-env',
+        {
+          targets: '> 0.25%, not dead',
+          modules: 'commonjs',
+          debug: true
+        }
+      ]
     ],
-    '@babel/typescript',
-    [
-      '@babel/preset-env',
-      {
-        targets: '> 0.25%, not dead',
-        modules: 'commonjs',
-        debug: true
-      }
-    ]
-  ]
-
-  return { presets, plugins: [] }
+    plugins: ['@babel/plugin-proposal-class-properties']
+  }
 }
