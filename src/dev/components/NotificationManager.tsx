09024f9b9a4c6a922b3038b4487c66f2528300ec
Mark Bevels <mark.bevels@derived.studio>
2021-03-17 02:43:58 +0000

    Cleanup

--- src/dev/components/NotificationManager.tsx
+++ src/dev/components/NotificationManager.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { removeNotification, updateNotification } from '../../lib/state'
 import { useNotifications } from '../../lib/notificationContext'
 
