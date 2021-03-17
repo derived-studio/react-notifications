1c7a45a602cc17486777c5bc50d35b4d29195d6c
Mark Bevels <mark.bevels@derived.studio>
2021-03-17 03:02:31 +0000

    Improved test readibility

--- src/lib/guards.spec.ts
+++ src/lib/guards.spec.ts
@@ -2,18 +2,12 @@ import { isNumber } from './guards'
 
 describe('Guards', () => {
   describe('is number guard', () => {
-    it('returns true for numbers', () => {
-      const numbers = [0, -10, 0.1, 1000, 1e3, -1e3]
-      numbers.forEach((val) => {
-        expect(isNumber(val)).toBeTruthy()
-      })
+    it.each([0, -10, 0.1, 1000, 1e3, -1e3])('is truthy for %s', (val) => {
+      expect(isNumber(val)).toBeTruthy()
     })
 
-    it('returns false for non numbers', () => {
-      const nonNumbers = [{}, '', '123', '123.2', '1e3', 'foo-bar', [], undefined, null]
-      nonNumbers.forEach((val) => {
-        expect(isNumber(val)).toBeFalsy()
-      })
+    it.each([{}, '', '123', '123.2', '1e3', 'foo-bar', [], undefined, null])('is falsy for %s', (val) => {
+      expect(isNumber(val)).toBeFalsy()
     })
   })
 })
